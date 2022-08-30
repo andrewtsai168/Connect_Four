339a9ca9e60ecdefb562aa114b66e5beb0e39952
Darby Smith <newman.darby2@gmail.com>
2022-08-30 17:51:55 -0400

    Initial commit

---
+++ spec/board_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require './lib/board'
+
+RSpec.describe Board do 
+  describe '#initialize' do 
+    it 'is an instance of '  
+  board = Board.new
+    expect(board).to be_instance_of Board
+  end 
+
+end 
