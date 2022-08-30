4b559f2553a918ba6da27d4d34f7dede0f95505a
Darby Smith <newman.darby2@gmail.com>
2022-08-30 18:43:08 -0400

    Test for grid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,22 @@ require './lib/board'
 
 RSpec.describe Board do 
   describe '#initialize' do 
-    it 'is an instance of '  
-  board = Board.new
-    expect(board).to be_instance_of Board
-  end 
+    it 'is an instance of board' do  
+      board = Board.new
+      expect(board).to be_instance_of Board
+    end
 
+    it 'has a grid' do 
+      board = Board.new 
+      expect(board.grid).to eq([
+        ["A", "B", "C", "D", "E", "F", "G"],
+        [".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", "."]
+      ])
+    end 
+  end 
 end 
