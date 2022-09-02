c2f4444d35d8e8eb8f40933941f8b22840fc4fa1
Darby Smith <newman.darby2@gmail.com>
2022-09-02 14:13:50 -0400

    Refactor column converter

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,46 +11,49 @@ class Turn
   def place_selection
     p "Choose a column A-G to drop your piece!"
     selection = gets.chomp.upcase
-    @turn_number += 1
 
     until @board.grid.first.include?(selection)
       p "Please input a column letter A-G."
       selection = gets.chomp.upcase
     end
-
-    column_check
-    add_piece_to_board
+    column_converter(selection)
   end
 
 
-  def column_converter
-    if selection == "A"
-      index_column = 0
-    elsif selection == "B"
-      index_column = 1
-    elsif selection == "C"
-      index_column = 2
-    elsif selection == "D"
-      index_column = 3
-    elsif selection == "E"
-      index_column = 4
-    elsif selection == "F"
-      index_column = 5
-    elsif selection == "G"
-      index_column = 6
-    end
+  def column_converter(selection)
+    selection_letters = @board.grid[0]
+    selection_letters.each.with_index do |l, index|
+      index_column = index if selection == l
+    end  
+    # if selection == "A"
+    #   index_column = 0 
+    # elsif selection == "B"
+    #   index_column = 1
+    # elsif selection == "C"
+    #   index_column = 2
+    # elsif selection == "D"
+    #   index_column = 3
+    # elsif selection == "E"
+    #   index_column = 4
+    # elsif selection == "F"
+    #   index_column = 5
+    # elsif selection == "G"
+    #   index_column = 6
+    # end
   end 
   
 
   def column_check(column_converter)
     index_row = 6
     column_contians = []
+    
     6.times do 
-      column_contains << grid[index_row][column_converter]
+      column_contains << @board.grid[index_row][column_converter]
       index_row -= 1
     end
     
     if column_contains.include?(".")
+      @turn_number += 1
       add_piece_to_board
     else
       p "This column is full. Please select another."
@@ -59,29 +62,22 @@ class Turn
   end
   
   
-  def add_piece_to_board 
+  def add_piece_to_board! 
     # place the piece in the bottom of the selected column 
-    if selection == @board.grid[0][0]
-      place_column_a
+    index_row = 6
+    until @board.grid[index_row][column_converter] == "."
+      index_row -= 1
     end
+    @board.grid[index][column_converter] = @player.piece
+    return @board.grid
     # display_board
   end
 
 
-  def place_column_a 
-    index_row = 6
-    until grid[index_row][column_converter]] == "."
-      index_row -= 1
-    end
-    grid[index][column_converter]] = @player.piece
-  end
-
-
-  def display_board
-    # this prints the new board after piece is placed
-  end
-
-
+  # def display_board
+  #   # this prints the new board after piece is placed
+  #   @board.grid.grid_format
+  # end
 end
 
 # grid[0][0] => "A" 
