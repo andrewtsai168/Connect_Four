b9c280a119f5e03c4027d851b0688e553c49d207
Darby Smith <newman.darby2@gmail.com>
2022-09-01 22:49:39 -0400

    Feat: selection converter

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,29 +11,62 @@ class Turn
   def place_selection
     p "Choose a column A-G to drop your piece!"
     selection = gets.chomp.upcase
-    
+    @turn_number += 1
+
     until @board.grid.first.include?(selection)
       p "Please input a column letter A-G."
       selection = gets.chomp.upcase
     end
-    change_board
+
+    column_check
+    add_piece_to_board
   end
 
+
+  def selection_converter
+    if selection == "A"
+      index_column = 0
+    elsif selection == "B"
+      index_column = 1
+    elsif selection == "C"
+      index_column = 2
+    elsif selection == "D"
+      index_column = 3
+    elsif selection == "E"
+      index_column = 4
+    elsif selection == "F"
+      index_column = 5
+    elsif selection == "G"
+      index_column = 6
+    end
+  end 
+  
+
+  def column_check(selection_converter)
+    # determines if column is full in order to move to piece placement
+    until grid[index_row][selection_converter] == "."
+      index = index_position - 1
+    end
+  end
+  
+  
   def add_piece_to_board 
     # place the piece in the bottom of the selected column 
     if selection == @board.grid[0][0]
       place_column_a
     end
-    display_board
+    # display_board
   end
 
+
   def place_column_a 
-    index_position = 6
-    while grid[index_position][0] == "."
-      index_position = index_position - 1
+    index_row = 6
+    until grid[index_row][selection_converter] == "."
+      index_row = index_position - 1
     end
-    
+    grid[index][selection_converter] = @player.piece
   end
+
 
   def display_board
     # this prints the new board after piece is placed
@@ -42,6 +75,6 @@ class Turn
 
 end
 
-grid[0][0] => "A"
-grid[6][0] => "X" => # don't place the piece here 
-grid[5][0] => "." => # place piece here
+# grid[0][0] => "A" 
+# grid[6][0] => "X" => don't place the piece here 
+# grid[5][0] => "." => place piece here
