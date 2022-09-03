32793a6a7044cae0676f411eb5c1e3a73722e61f
Andrew Tsai <a168tsai@gmail.com>
2022-09-03 15:02:01 -0600

    Feat: main menu

--- lib/game.rb
+++ lib/game.rb
@@ -7,4 +7,24 @@ class Game
     @computer = computer
   end
 
+  def welcome
+     "Welcome to Connect 4!"
+  end
+
+  def main_menu
+    puts "Enter p to play. Enter q to quit."
+    play_choice = gets.chomp.upcase
+    if play_choice == "P"
+      @turn.display_board
+      @turn.place_selection
+    elsif play_choice == "Q"
+      puts "Please Don't Leave..."
+      puts "Enter p to play. Enter q to quit."
+      play_choice = gets.chomp.upcase
+    else
+      puts "Enter p to play. Enter q to quit."
+      play_choice = gets.chomp.upcase
+    end
+  end
+
 end
